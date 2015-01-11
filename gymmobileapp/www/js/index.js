e2fb20810050b65afe6e7079d1aa573c36aceab4
scott mcwee <s.mcwee@hotmail.co.uk>
2015-01-11 11:46:03 +0000

    index.js update file

--- gymmobileapp/www/js/index.js
+++ gymmobileapp/www/js/index.js
@@ -1,9 +1,7 @@
-// create a new gym-task
 function createNewGymTask()
 {
     var gymtaskDictionary = {};
- 
-    // prompt the user to enter gym-task
+	
     var gymtask = prompt("What new gym-task would you like to add?","");
     if (gymtask != null)
     {
@@ -13,83 +11,72 @@ function createNewGymTask()
         }
         else
         {
-            // append the new gym-task with the table
             gymtaskDictionary = { check : 0 , text : gymtask};
-            addTableRow(gymtaskDictionary, false);
+            addGymTaskTableRow(gymtaskDictionary, false);
         }
     }
- 
 }
-// add a row to the table
 var rowID = 0;
-function addTableRow(gymtaskDictionary, appIsLoading)
+function addGymTaskTableRow(gymtaskDictionary, appIsLoading)
 {
     rowID +=1;
     var table = document.getElementById("dataTable");
     var rowCount = table.rows.length;
     var row = table.insertRow(rowCount);
  
-    // create the checkbox
-    var cell1 = row.insertCell(0);
-    var element1 = document.createElement("input");
-    element1.type = "checkbox";
-    element1.name = "chkbox[]";
-    element1.checked = gymtaskDictionary["check"];
-    element1.setAttribute("onclick", "checkboxClicked()");
-	element1.className = "checkbox";
-    cell1.appendChild(element1);
+    var gymtaskcheckboxcell = row.insertCell(0);
+    var gymtaskcheckbox = document.createElement("input");
+    gymtaskcheckbox.type = "checkbox";
+    gymtaskcheckbox.name = "chkbox[]";
+    gymtaskcheckbox.checked = gymtaskDictionary["check"];
+    gymtaskcheckbox.setAttribute("onclick", "gymtaskcheckboxClicked()");
+	gymtaskcheckbox.className = "gymtaskcheckbox";
+    gymtaskcheckboxcell.appendChild(gymtaskcheckbox);
  
-    // create the textbox
-    var cell2 = row.insertCell(1);
-    var element2 = document.createElement("input");
-    element2.type = "text";
-    element2.name = "txtbox[]";
-    element2.size = 16;
-    element2.id = "text" + rowID;
-    element2.value = gymtaskDictionary["text"];
-    element2.setAttribute("onchange", "saveGymTaskList()");
-	element2.className = "textbox";
-    cell2.appendChild(element2);
+    var gymtasktextboxcell = row.insertCell(1);
+    var gymtasktextbox = document.createElement("input");
+    gymtasktextbox.type = "text";
+    gymtasktextbox.name = "txtbox[]";
+    gymtasktextbox.size = 16;
+    gymtasktextbox.id = "text" + rowID;
+    gymtasktextbox.value = gymtaskDictionary["text"];
+    gymtasktextbox.setAttribute("onchange", "saveGymTaskList()");
+	gymtasktextbox.className = "gymtasktextbox";
+    gymtasktextboxcell.appendChild(gymtasktextbox);
  
-    // create the view button
-    var cell3 = row.insertCell(2);
-    var element3 = document.createElement("input");
-    element3.type = "button";
-    element3.id = rowID;
-    element3.value = "View";
-    element3.setAttribute("onclick", "viewSelectedRow(document.getElementById('text' + this.id))");
-	element3.className = "viewButton";
-    cell3.appendChild(element3);
- 
-    // create the delete button
-    var cell4 = row.insertCell(3);
-    var element4 = document.createElement("input");
-    element4.type = "button";
-    element4.value = "Delete";
-    element4.setAttribute("onclick", "deleteSelectedRow(this)");
-	element4.className = "deleteButton";
-    cell4.appendChild(element4);
- 
-    // update the UI and save the gym-task list
-    checkboxClicked();
+    var gymtaskviewbuttoncell = row.insertCell(2);
+    var gymtaskviewbutton = document.createElement("input");
+    gymtaskviewbutton.type = "button";
+    gymtaskviewbutton.id = rowID;
+    gymtaskviewbutton.value = "View";
+    gymtaskviewbutton.setAttribute("onclick", "viewSelectedRow(document.getElementById('text' + this.id))");
+	gymtaskviewbutton.className = "gymtaskviewButton";
+    gymtaskviewbuttoncell.appendChild(gymtaskviewbutton);
+
+    var gymtaskdeletebuttoncell = row.insertCell(3);
+    var gymtaskdeletebutton = document.createElement("input");
+    gymtaskdeletebutton.type = "button";
+    gymtaskdeletebutton.value = "Delete";
+    gymtaskdeletebutton.setAttribute("onclick", "deleteSelectedRow(this)");
+	gymtaskdeletebutton.className = "gymtaskdeleteButton";
+    gymtaskdeletebuttoncell.appendChild(gymtaskdeletebutton);
+
+    gymtaskcheckboxClicked();
     saveGymTaskList();
  
     if (!appIsLoading) alert("Your Gym-Task has been Added Successfully!");
 }
-// add the strike-through styling to completed tasks
-function checkboxClicked()
+function gymtaskcheckboxClicked()
 {
     var table = document.getElementById("dataTable");
     var rowCount = table.rows.length;
  
-    // loop through all rows of the table
     for(var i = 0; i < rowCount; i++)
     {
         var row = table.rows[i];
         var chkbox = row.cells[0].childNodes[0];
         var textbox = row.cells[1].childNodes[0];
  
-        // if the checkbox is checked, add the strike-through styling
         if(null != chkbox && true == chkbox.checked)
         {
             if(null != textbox)
@@ -97,40 +84,31 @@ function checkboxClicked()
                 textbox.style.setProperty("text-decoration", "line-through");
             }
         }
- 
-        // if the checkbox isn't checked, remove the strike-through styling
+
         else
         {
             textbox.style.setProperty("text-decoration", "none");
         }
- 
     }
- 
-    // save the gym-task list
     saveGymTaskList();
 }
-// view the content of the selected row
 function viewSelectedRow(gymtaskTextField)
 {
     alert(gymtaskTextField.value);
 }
-// delete the selected row
 function deleteSelectedRow(deleteButton)
 {
     var p = deleteButton.parentNode.parentNode;
     p.parentNode.removeChild(p);
     saveGymTaskList();
 }
-// remove completed tasks
 function removeCompletedTasks()
 {
     var table = document.getElementById("dataTable");
     var rowCount = table.rows.length;
  
-    // loop through all rows of the table
     for(var i = 0; i < rowCount; i++)
     {
-        // if the checkbox is checked, delete the row
         var row = table.rows[i];
         var chkbox = row.cells[0].childNodes[0];
         if(null != chkbox && true == chkbox.checked)
@@ -141,12 +119,10 @@ function removeCompletedTasks()
         }
     }
  
-    // save the gym-task list
     saveGymTaskList();
  
     alert("Completed have been successfully removed.");
 }
-// save the gym-task list
 function saveGymTaskList()
 {
     var gymtaskArray = {};
@@ -158,12 +134,10 @@ function saveGymTaskList()
  
     if (rowCount != 0)
     {
-        // loop through all rows of the table
         for(var i=0; i<rowCount; i++)
         {
             var row = table.rows[i];
  
-            // determine the state of the checkbox
             var chkbox = row.cells[0].childNodes[0];
             if(null != chkbox && true == chkbox.checked)
             {
@@ -174,11 +148,9 @@ function saveGymTaskList()
                 checkBoxState= 0;
             }
  
-            // retrieve the content of the gym-task
             var textbox = row.cells[1].childNodes[0];
             textValue = textbox.value;
  
-            // populate the array
             gymtaskArray["row" + i] =
             {
                 check : checkBoxState,
@@ -191,18 +163,15 @@ function saveGymTaskList()
         gymtaskArray = null;
     }
  
-    // use the local storage API to persist the data as JSON
     window.localStorage.setItem("gymtaskList", JSON.stringify(gymtaskArray));
 }
-// load the gym-task list
 function loadGymTaskList()
 {
-    // use the local storage API load the JSON formatted gym-task list, and decode it
     var theList = JSON.parse(window.localStorage.getItem("gymtaskList"));
  
     if (null == theList || theList == "null")
     {
-        deleteAllRows();
+        deleteAllGymTaskRows();
 		alert("You have no current tasks saved.");
     }
     else
@@ -213,32 +182,25 @@ function loadGymTaskList()
             count++;
         }
  
-        // remove any existing rows from the table
-        deleteAllRows();
+        deleteAllGymTaskRows();
  
-        // loop through the gym-tasks
         for(var i = 0; i < count; i++)
         {
-            // adding a row to the table for each one
-            addTableRow(theList["row" + i], true);
+            addGymTaskTableRow(theList["row" + i], true);
         }
     }
 }
-// delete all the rows
-function deleteAllRows()
+function deleteAllGymTaskRows()
 {
     var table = document.getElementById("dataTable");
     var rowCount = table.rows.length;
  
-    // loop through all rows of the table
     for(var i = 0; i < rowCount; i++)
     {
-        // delete the row
         table.deleteRow(i);
         rowCount--;
         i--;
     }
  
-    // save the gym-task list
     saveGymTaskList();
 }
